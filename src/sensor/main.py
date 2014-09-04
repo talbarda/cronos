4fadb7f80bb4c8ad6a8da9c5754fd87b487df482
chronosx <cenix.stylo@gmail.com>
2014-09-04 21:17:55 +0000

    Added gettemp and getpressure

--- src/sensor/main.py
+++ src/sensor/main.py
@@ -10,8 +10,19 @@ INTERVAL = get_conf().get_conf_value(full_name="sensor.update_seconds_interval",
 def collect_data():
     return dict(ts=str(get_current_time()),
                 uid=str(uuid.uuid1()),
-                temparture=-5 + (45*random.random()),
-                pressure=300 + (400*random.random()))
+                temparture=get_temp(),
+                pressure=get_pressure())
+
+def get_temp():
+    x = grove.GroveTemp(0)
+    a = x.raw_value()
+    a = int(a) >> 2
+    r = float((1023-a)*10000/a)
+    return 1.7*(1/(math.log(r/10000)/3975 + 1/298.15)-273.15)
+
+def get_pressure():
+    x = mraa.Aio(2)
+    return x.read()
 
 def handle_response(response_val):
     print response_val
