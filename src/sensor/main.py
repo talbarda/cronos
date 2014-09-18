a87c2eaf6fe8dadaf1b9f07fa53d9c273937df6e
talbarda <talbarda88+github@gmail.com>
2014-09-19 00:16:34 +0300

    implemented basic light response

--- src/sensor/main.py
+++ src/sensor/main.py
@@ -1,3 +1,4 @@
+import json
 import sys
 
 __author__ = 'talbarda'
@@ -11,25 +12,40 @@ import pyupm_grove as grove
 import math
 
 INTERVAL = get_conf().get_conf_value(full_name="sensor.update_seconds_interval", val_type=float, def_val=1)
+temperature_sensor = grove.GroveTemp(0)
+pressure_sensor = mraa.Aio(2)
+light = mraa.Gpio(2)
+light.dir(mraa.DIR_OUT)
+light_value = 0
+light.write(light_value)
+
+def change_light(val):
+    global light_value
+    if light_value!=val:
+        light_value=val
+        light.write(val)
 
 def collect_data():
     return dict(ts=str(get_current_time()),
                 uid=str(uuid.uuid1()),
-                temparture=get_temp(),
+                temperature=get_temp(),
                 pressure=get_pressure())
 
 def get_temp():
-    x = grove.GroveTemp(0)
-    a = x.raw_value()
+    a = temperature_sensor.raw_value()
     a = int(a) >> 2
     r = float((1023-a)*10000/a)
     return 1.7*(1/(math.log(r/10000)/3975 + 1/298.15)-273.15)
 
 def get_pressure():
-    x = mraa.Aio(2)
-    return x.read()
+    return pressure_sensor.read()
 
 def handle_response(response_val):
+    response_dict = json.loads(response_val)
+    if response_dict["temperature"]>=20:
+        change_light(1)
+    else:
+        change_light(0)
     print response_val
 
 if __name__ == "__main__":
